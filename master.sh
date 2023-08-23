82088f08d619c4602b7434e60724efc1dff8a21e
jioyla <lz4261@gmail.com>
2023-08-23 15:55:16 +0800

    55

--- master.sh
+++ master.sh
@@ -7,12 +7,14 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
+cd docs/.vuepress/dist
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jioyla/jioyla.github.io.git main
+git push -f https://github.com/jioyla/jioyla.github.io.git main:gh-pages
 
 cd -
 
